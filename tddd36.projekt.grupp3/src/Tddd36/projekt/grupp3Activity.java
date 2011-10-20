11ae9c66ec398014b48a7181b2e04611799a444d
David Buö <davidbauwie@gmail.com>
2011-10-20 08:50:20 +0200

    TJa

---
+++ tddd36.projekt.grupp3/src/Tddd36/projekt/grupp3Activity.java
@@ -0,0 +1,14 @@
+package Tddd36.projekt;
+
+import grupp3.paket.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class grupp3Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
